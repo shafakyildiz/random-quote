138c98568cdb4a48b3070999cc5c9080877481f8
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-20 11:11:16 +0300

    Quote to the console

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=1')
   .then(function(response) {
-    console.log(response);
     let h2 = document.getElementById('main');
     h2.innerText = response.status;
-  })
+    return response.json();
+  }).then(response => (console.log(response.quotes[0].text)))
   .catch(function(err) {
     console.log("Something went wrong!", err);
   });
-
