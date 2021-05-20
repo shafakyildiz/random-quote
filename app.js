4000fcfe76097753da0151b5336b5ce6e9627b3d
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-20 11:31:00 +0300

    Working software

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=1')
     let h2 = document.getElementById('main');
     h2.innerText = response.status;
     return response.json();
-  }).then(response => (console.log(response.quotes[0].text)))
-  .catch(function(err) {
+  }).then(response => {
+      console.log(response);
+      console.log(response.quotes[0].text)
+      let h2 = document.getElementById('main');
+      h2.innerText = response.quotes[0].text;
+    })
+    .catch(function(err) {
     console.log("Something went wrong!", err);
   });
