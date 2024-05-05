0ec061605a58c16a9663dddd78d8791302c15b5e
shafakyildiz <shafakyildiz@gmail.com>
2024-05-05 14:04:49 +0300

    change index

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=1')
-  .then(function(response) {
+fetch('https://api.api-ninjas.com/v1/quotes?category=movies')
+  .then(function (response) {
     let h2 = document.getElementById('main');
     h2.innerText = response.status;
     return response.json();
   }).then(response => {
-      console.log(response);
-      console.log(response.quotes[0].text)
-      let h2 = document.getElementById('main');
-      h2.innerText = response.quotes[0].text;
-    })
-    .catch(function(err) {
+    console.log(response);
+    console.log(response.quotes[0].text)
+    let h2 = document.getElementById('main');
+    h2.innerText = response.quotes[0].text;
+  })
+  .catch(function (err) {
     console.log("Something went wrong!", err);
   });
