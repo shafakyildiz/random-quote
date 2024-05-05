0ff164c4abaf84033cc935d0815201894f6cfb83
shafakyildiz <shafakyildiz@gmail.com>
2024-05-05 14:16:24 +0300

    remove comment

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// fetch('https://api.api-ninjas.com/v1/quotes?category=movies')
-//   .then(function (response) {
-//     let quoteBox = document.getElementById('quote-box');
-//     quoteBox.innerText = response.status;
-//     return response.json();
-//   }).then(response => {
-//     console.log(response);
-//     console.log(response.quotes[0].text)
-//     let h2 = document.getElementById('main');
-//     h2.innerText = response.quotes[0].text;
-//   })
-//   .catch(function (err) {
-//     console.log("Something went wrong!", err);
-//   });
-
 const url = "https://api.api-ninjas.com/v1/quotes?category=movies";
 fetch(url, {
   method: "GET",
