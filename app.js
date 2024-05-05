fef5cae8a20abc0ecdc234cb218ec05f2137b709
shafakyildiz <shafakyildiz@gmail.com>
2024-05-05 14:24:01 +0300

    minor changes

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const url = "https://api.api-ninjas.com/v1/quotes?category=movies";
-fetch(url, {
-  method: "GET",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "X-Api-Key": "PJAAKHnO8WYE17tFKMTM1A==762bz2jOUe8aO8p1"
-  }
-})
-  .then(resp => resp.json())
-  .then(function (data) {
-    console.log(data[0]);
-    let quote = data[0];
-    let quoteText = document.getElementById('text');
-    quoteText.innerText = quote.quote;
-    let authorText = document.getElementById('author');
-    authorText.innerText = quote.author;
+
+fetchQuote();
+
+function fetchQuote() {
+  fetch(url, {
+    method: "GET",
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": "PJAAKHnO8WYE17tFKMTM1A==762bz2jOUe8aO8p1"
+    }
   })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(resp => resp.json())
+    .then(function (data) {
+      console.log(data[0]);
+      let quote = data[0];
+      let quoteText = document.getElementById('text');
+      quoteText.innerText = quote.quote;
+      let authorText = document.getElementById('author');
+      authorText.innerText = quote.author;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
